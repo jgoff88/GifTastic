b7a6571cb232a3926851f1b7d7337950d40f0692
James Goff <james_goff88@outlook.com>
2019-10-31 19:07:05 -0500

    add js, html, css, and image file. Add apikey and console log response.

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+        // Example queryURL for Giphy API
+        var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=RhoHUzXl6SP5fFtT3qBCsi2mOMC53BaJ";
+
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function(response) {
+          console.log(response);
+        });
+
+});
